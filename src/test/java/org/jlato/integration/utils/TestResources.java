c8be7906634c927493cf9074b5e1d400e2557a2b
Didier Villevalois <ptitjes@free.fr>
2015-07-07 17:25:33 +0200

    Add LGPL License

--- src/test/java/org/jlato/integration/utils/TestResources.java
+++ src/test/java/org/jlato/integration/utils/TestResources.java
@@ -1,21 +1,20 @@
 /*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2015 The JavaParser Team.
+ * Copyright (C) 2015 Didier Villevalois.
  *
- * This file is part of JavaParser.
+ * This file is part of JLaTo.
  *
- * JavaParser is free software: you can redistribute it and/or modify
+ * JLaTo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JavaParser is distributed in the hope that it will be useful,
+ * JLaTo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with JavaParser.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JLaTo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.jlato.integration.utils;
