b083ad28ef7b2a7d3825271a3d1f37b6e5addaf4
Didier Villevalois <ptitjes@free.fr>
2015-07-10 14:24:04 +0200

    Some more work to enRun grammar

--- src/main/java/org/jlato/tree/type/ArrayType.java
+++ src/main/java/org/jlato/tree/type/ArrayType.java
@@ -19,7 +19,6 @@
 
 package org.jlato.tree.type;
 
-import org.jlato.internal.bu.LToken;
 import org.jlato.internal.bu.SNode;
 import org.jlato.internal.bu.SNodeState;
 import org.jlato.internal.shapes.LexicalShape;
@@ -27,6 +26,8 @@ import org.jlato.tree.NodeList;
 import org.jlato.tree.SLocation;
 import org.jlato.tree.Tree;
 import org.jlato.tree.Type;
+import org.jlato.tree.decl.ArrayDim;
+import org.jlato.tree.decl.VariableDeclaratorId;
 import org.jlato.tree.expr.AnnotationExpr;
 
 import static org.jlato.internal.shapes.LexicalShape.Factory.*;
@@ -47,8 +48,8 @@ public class ArrayType extends ReferenceType {
 		super(location);
 	}
 
-	public ArrayType(NodeList<AnnotationExpr> annotations, Type componentType) {
-		super(new SLocation(new SNode(kind, new SNodeState(treesOf(annotations, componentType)))));
+	public ArrayType(NodeList<AnnotationExpr> annotations, Type componentType, NodeList<ArrayDim> dimensions) {
+		super(new SLocation(new SNode(kind, new SNodeState(treesOf(annotations, componentType, dimensions)))));
 	}
 
 	public Type componentType() {
@@ -59,11 +60,20 @@ public class ArrayType extends ReferenceType {
 		return location.nodeWithChild(COMPONENT_TYPE, componentType);
 	}
 
+	public NodeList<ArrayDim> dimensions() {
+		return location.nodeChild(DIMENSIONS);
+	}
+
+	public VariableDeclaratorId withDimensions(NodeList<ArrayDim> dimensions) {
+		return location.nodeWithChild(DIMENSIONS, dimensions);
+	}
+
 	private static final int COMPONENT_TYPE = 1;
+	private static final int DIMENSIONS = 1;
 
 	public final static LexicalShape shape = composite(
 			children(ANNOTATIONS),
 			child(COMPONENT_TYPE),
-			token(LToken.BracketLeft), token(LToken.BracketRight)
+			children(DIMENSIONS)
 	);
 }
