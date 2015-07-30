07a80776fb1e2d5786034be774c138f4dac93689
Didier Villevalois <ptitjes@free.fr>
2015-07-30 11:56:38 +0200

    [Lex] Fix WRunRun.insertBefore()

--- src/main/java/org/jlato/internal/bu/WRunRun.java
+++ src/main/java/org/jlato/internal/bu/WRunRun.java
@@ -33,52 +33,36 @@ public class WRunRun extends WRun {
 	}
 
 	public WRunRun insertBefore(int index, int count) {
-		if (index < 1 || index > (elements.size() + 1) / 2)
+		if (index < 1 || index >= (elements.size() + 1) / 2)
 			throw new IllegalArgumentException();
 		if (count < 1)
 			throw new IllegalArgumentException();
 
-		if (index == 0) {
-			IndexedList<WRun> newElements = elements;
-			for (int i = 0; i < count; i++) {
-				// Prepend a sub-shape run and a whitespaces run
-				newElements = newElements.prepend(null).prepend(null);
-			}
-			return new WRunRun(newElements);
-		} else if (index == (elements.size() + 1) / 2) {
-			IndexedList<WRun> newElements = elements;
-			for (int i = 0; i < count; i++) {
-				// Append a whitespaces run and a sub-shape run
-				newElements = newElements.append(null).append(null);
-			}
-			return new WRunRun(newElements);
-		} else {
-			int cutIndex = index * 2 - 1;
+		int cutIndex = index * 2 - 1;
 
-			WRun whitespace = elements.get(cutIndex);
-			IndexedList<WRun> left = elements.take(cutIndex);
-			IndexedList<WRun> right = elements.drop(cutIndex + 1);
+		WRun whitespace = elements.get(cutIndex);
+		IndexedList<WRun> left = elements.take(cutIndex);
+		IndexedList<WRun> right = elements.drop(cutIndex + 1);
 
-			// Split the whitespace run
-			// TODO Implement splitting the whitespaces with user involvement ?
-			WRun leftSplit = null;
-			WRun rightSplit = whitespace;
+		// Split the whitespace run
+		// TODO Implement splitting the whitespaces with user involvement ?
+		WRun leftSplit = null;
+		WRun rightSplit = whitespace;
 
-			// Append the left split whitespaces run and a sub-shape run
-			IndexedList<WRun> newElements = left.append(leftSplit).append(null);
+		// Append the left split whitespaces run and a sub-shape run
+		IndexedList<WRun> newElements = left.append(leftSplit).append(null);
 
-			for (int i = 0; i < count - 1; i++) {
-				// Append a whitespaces run and a sub-shape run
-				newElements = newElements.append(null).append(null);
-			}
-			// Append the right split whitespaces run
-			newElements = newElements.append(rightSplit);
-
-			for (WRun element : right) {
-				newElements = newElements.append(element);
-			}
-			return new WRunRun(newElements);
+		for (int i = 0; i < count - 1; i++) {
+			// Append a whitespaces run and a sub-shape run
+			newElements = newElements.append(null).append(null);
 		}
+		// Append the right split whitespaces run
+		newElements = newElements.append(rightSplit);
+
+		for (WRun element : right) {
+			newElements = newElements.append(element);
+		}
+		return new WRunRun(newElements);
 	}
 
 	@Override
