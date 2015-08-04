44df9a824dd902ec728fd0a058e6ddbb3f54e4b6
Didier Villevalois <ptitjes@free.fr>
2015-08-04 12:52:57 +0200

    [AST] Forgot Modifier static fields

--- src/main/java/org/jlato/tree/decl/Modifier.java
+++ src/main/java/org/jlato/tree/decl/Modifier.java
@@ -1,9 +1,34 @@
 package org.jlato.tree.decl;
 
+import org.jlato.internal.td.decl.TDModifier;
 import org.jlato.tree.TreeCombinators;
 import org.jlato.util.Mutation;
 
 public interface Modifier extends ExtendedModifier, TreeCombinators<Modifier> {
+
+	Modifier Public = new TDModifier(ModifierKeyword.Public);
+
+	Modifier Protected = new TDModifier(ModifierKeyword.Protected);
+
+	Modifier Private = new TDModifier(ModifierKeyword.Private);
+
+	Modifier Abstract = new TDModifier(ModifierKeyword.Abstract);
+
+	Modifier Default = new TDModifier(ModifierKeyword.Default);
+
+	Modifier Static = new TDModifier(ModifierKeyword.Static);
+
+	Modifier Final = new TDModifier(ModifierKeyword.Final);
+
+	Modifier Transient = new TDModifier(ModifierKeyword.Transient);
+
+	Modifier Volatile = new TDModifier(ModifierKeyword.Volatile);
+
+	Modifier Synchronized = new TDModifier(ModifierKeyword.Synchronized);
+
+	Modifier Native = new TDModifier(ModifierKeyword.Native);
+
+	Modifier StrictFP = new TDModifier(ModifierKeyword.StrictFP);
 
 	ModifierKeyword keyword();
 
