c17dabe6bb4f7043f173111d54d1363829d87281
Didier Villevalois <ptitjes@free.fr>
2015-08-07 19:09:56 +0200

    [Test] Add tests for literals and move to manually handled unicode escape

--- src/main/java/org/jlato/internal/bu/Literals.java
+++ src/main/java/org/jlato/internal/bu/Literals.java
@@ -31,26 +31,67 @@ public class Literals {
 				throw new IllegalArgumentException();
 			}
 			return LToken.Null.string;
+
 		} else if (Boolean.class.isAssignableFrom(literalClass)) {
 			if (Boolean.TRUE.equals(literalValue))
 				return LToken.True.string;
 			else if (Boolean.FALSE.equals(literalValue))
 				return LToken.False.string;
 			else throw new IllegalStateException();
+
 		} else if (Character.class.isAssignableFrom(literalClass)) {
-			return "'" + literalValue.toString().charAt(0) + "'";
+			char c = (char) (Character) literalValue;
+			return "'" + escapeChar(c) + "'";
+
 		} else if (Integer.class.isAssignableFrom(literalClass)) {
 			return literalValue.toString();
+
 		} else if (Long.class.isAssignableFrom(literalClass)) {
 			return literalValue.toString() + "L";
+
 		} else if (Float.class.isAssignableFrom(literalClass)) {
 			return literalValue.toString() + "F";
+
 		} else if (Double.class.isAssignableFrom(literalClass)) {
-			return literalValue.toString() + "D";
+			return literalValue.toString();
+
 		} else if (String.class.isAssignableFrom(literalClass)) {
-			return "\"" + literalValue.toString() + "\"";
+			return "\"" + escapeString((String) literalValue) + "\"";
+
 		} else {
 			throw new IllegalArgumentException();
+		}
+	}
+
+	private static String escapeString(String string) {
+		StringBuilder buffer = new StringBuilder();
+		char[] chars = string.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			buffer.append(escapeChar(chars[i]));
+		}
+		return buffer.toString();
+	}
+
+	private static String escapeChar(char c) {
+		switch (c) {
+			case '\t':
+				return "\\t";
+			case '\b':
+				return "\\b";
+			case '\n':
+				return "\\n";
+			case '\r':
+				return "\\r";
+			case '\f':
+				return "\\f";
+			case '\'':
+				return "\\'";
+			case '\"':
+				return "\\\"";
+			case '\\':
+				return "\\\\";
+			default:
+				return "" + c;
 		}
 	}
 
@@ -58,26 +99,296 @@ public class Literals {
 	public static <T> T valueFor(Class<T> literalClass, String literalString) {
 		if (Void.class.isAssignableFrom(literalClass)) {
 			return null;
+
 		} else if (Boolean.class.isAssignableFrom(literalClass)) {
 			if (literalString.equals("true"))
 				return (T) Boolean.TRUE;
-			else if (literalString.equals("true"))
+			else if (literalString.equals("false"))
 				return (T) Boolean.FALSE;
 			else throw new IllegalStateException();
+
 		} else if (Character.class.isAssignableFrom(literalClass)) {
-			return (T) (Character) literalString.charAt(1);
+			if (literalString.startsWith("'\\")) {
+				char[] chars = literalString.toCharArray();
+				return (T) Character.valueOf(unEscapeChar(chars, 1));
+			} else return (T) Character.valueOf(literalString.charAt(1));
+
 		} else if (Integer.class.isAssignableFrom(literalClass)) {
-			return (T) (Integer) Integer.parseInt(literalString);
+			literalString = removeUnderscores(literalString);
+
+			if (literalString.startsWith("0x") || literalString.startsWith("0X"))
+				return (T) Integer.valueOf(removePrefix(literalString, 2), 16);
+			else if (literalString.startsWith("0b") || literalString.startsWith("0B"))
+				return (T) Integer.valueOf(removePrefix(literalString, 2), 2);
+			else if (literalString.startsWith("0") || literalString.startsWith("0"))
+				return (T) Integer.valueOf(removePrefix(literalString, 1), 8);
+			else return (T) Integer.valueOf(literalString);
+
 		} else if (Long.class.isAssignableFrom(literalClass)) {
-			return (T) (Long) Long.parseLong(literalString);
+			literalString = removeUnderscores(literalString);
+			literalString = removeSuffix(literalString, 1);
+
+			if (literalString.startsWith("0x") || literalString.startsWith("0X"))
+				return (T) Long.valueOf(removePrefix(literalString, 2), 16);
+			else if (literalString.startsWith("0b") || literalString.startsWith("0B"))
+				return (T) Long.valueOf(removePrefix(literalString, 2), 2);
+			else if (literalString.startsWith("0") || literalString.startsWith("0"))
+				return (T) Long.valueOf(removePrefix(literalString, 1), 8);
+			return (T) Long.valueOf(literalString);
+
 		} else if (Float.class.isAssignableFrom(literalClass)) {
-			return (T) (Float) Float.parseFloat(literalString);
+			return (T) Float.valueOf(literalString);
+
 		} else if (Double.class.isAssignableFrom(literalClass)) {
-			return (T) (Double) Double.parseDouble(literalString);
+			return (T) Double.valueOf(literalString);
+
 		} else if (String.class.isAssignableFrom(literalClass)) {
-			return (T) literalString.substring(1, literalString.length() - 1);
+			return (T) unEscapeString(literalString);
+
 		} else {
 			throw new IllegalStateException();
 		}
+	}
+
+	private static String removePrefix(String string, int length) {
+		return string.substring(length);
+	}
+
+	private static String removeSuffix(String string, int length) {
+		return string.substring(0, string.length() - length);
+	}
+
+	private static String removeUnderscores(String literalString) {
+		literalString = literalString.replace("_", "");
+		return literalString;
+	}
+
+	public static String unEscapeString(String string) {
+		StringBuilder buffer = new StringBuilder();
+		char[] chars = string.toCharArray();
+		for (int i = 1; i < chars.length - 1; ) {
+			buffer.append(unEscapeChar(chars, i));
+			i += escapedCharLength(chars, i);
+		}
+		return buffer.toString();
+	}
+
+	public static char unEscapeChar(char[] chars, int index) {
+		switch (chars[index]) {
+			case '\\':
+				if (chars.length == index + 1) {
+					throw new IllegalArgumentException("Unknown char escape '" + chars[index] + "'");
+				}
+				switch (chars[index + 1]) {
+					case 't':
+						return '\t';
+					case 'b':
+						return '\b';
+					case 'n':
+						return '\n';
+					case 'r':
+						return '\r';
+					case 'f':
+						return '\f';
+					case '\'':
+						return '\'';
+					case '"':
+						return '\"';
+					case '\\':
+						return '\\';
+					case 'u':
+						// TODO This is incorrect - unicode escape should happen earlier
+						return readUnicodeChar(chars, index + 2);
+					default:
+						int codePoint;
+						boolean firstZeroToThree = false;
+						switch (chars[index + 1]) {
+							case '0':
+							case '1':
+							case '2':
+							case '3':
+								firstZeroToThree = true;
+							case '4':
+							case '5':
+							case '6':
+							case '7':
+								codePoint = Character.digit(chars[index + 1], 8);
+								if (chars.length > index + 2) {
+									switch (chars[index + 2]) {
+										case '0':
+										case '1':
+										case '2':
+										case '3':
+										case '4':
+										case '5':
+										case '6':
+										case '7':
+											codePoint = codePoint * 8 + Character.digit(chars[index + 2], 8);
+											if (chars.length > index + 3) {
+												switch (chars[index + 3]) {
+													case '0':
+													case '1':
+													case '2':
+													case '3':
+													case '4':
+													case '5':
+													case '6':
+													case '7':
+														if (!firstZeroToThree) {
+															throw new IllegalArgumentException("Unknown char escape '" +
+																	chars[index] + chars[index + 1] +
+																	chars[index + 2] + chars[index + 3] + "'");
+														}
+														codePoint = codePoint * 8 + Character.digit(chars[index + 3], 8);
+													default:
+												}
+											}
+										default:
+									}
+								}
+								return Character.toChars(codePoint)[0];
+							default:
+						}
+
+						throw new IllegalArgumentException("Unknown char escape '" + chars[index] + chars[index + 1] + "'");
+				}
+			default:
+				return chars[index];
+		}
+	}
+
+	private static char escapedCharLength(char[] value, int index) {
+		switch (value[index]) {
+			case '\\':
+				if (value.length < index + 1) {
+					throw new IllegalArgumentException("Unknown char escape '" + value[index] + "'");
+				}
+				switch (value[index + 1]) {
+					case 't':
+						return 2;
+					case 'b':
+						return 2;
+					case 'n':
+						return 2;
+					case 'r':
+						return 2;
+					case 'f':
+						return 2;
+					case '\'':
+						return 2;
+					case '"':
+						return 2;
+					case '\\':
+						return 2;
+					case 'u':
+						// TODO This is incorrect - unicode escape should happen earlier
+						return 6;
+					default:
+						boolean firstZeroToThree = false;
+						switch (value[index + 1]) {
+							case '0':
+							case '1':
+							case '2':
+							case '3':
+								firstZeroToThree = true;
+							case '4':
+							case '5':
+							case '6':
+							case '7':
+								if (value.length > index + 2) {
+									switch (value[index + 2]) {
+										case '0':
+										case '1':
+										case '2':
+										case '3':
+										case '4':
+										case '5':
+										case '6':
+										case '7':
+											if (value.length > index + 3) {
+												switch (value[index + 3]) {
+													case '0':
+													case '1':
+													case '2':
+													case '3':
+													case '4':
+													case '5':
+													case '6':
+													case '7':
+														if (!firstZeroToThree) {
+															throw new IllegalArgumentException("Unknown char escape '" +
+																	value[index] + value[index + 1] +
+																	value[index + 2] + value[index + 3] + "'");
+														}
+														return 4;
+													default:
+												}
+											}
+											return 3;
+										default:
+									}
+								}
+								return 2;
+							default:
+						}
+						throw new IllegalArgumentException("Unknown char escape '" + value[index] + value[index + 1] + "'");
+				}
+			default:
+				return 1;
+		}
+	}
+
+	private static char readUnicodeChar(char[] chars, int offset) {
+		return (char)
+				(hexValue(chars[offset]) << 12 |
+						hexValue(chars[offset + 1]) << 8 |
+						hexValue(chars[offset + 2]) << 4 |
+						hexValue(chars[offset + 3]));
+	}
+
+	private static int hexValue(char c) {
+		switch (c) {
+			case '0':
+				return 0;
+			case '1':
+				return 1;
+			case '2':
+				return 2;
+			case '3':
+				return 3;
+			case '4':
+				return 4;
+			case '5':
+				return 5;
+			case '6':
+				return 6;
+			case '7':
+				return 7;
+			case '8':
+				return 8;
+			case '9':
+				return 9;
+
+			case 'a':
+			case 'A':
+				return 10;
+			case 'b':
+			case 'B':
+				return 11;
+			case 'c':
+			case 'C':
+				return 12;
+			case 'd':
+			case 'D':
+				return 13;
+			case 'e':
+			case 'E':
+				return 14;
+			case 'f':
+			case 'F':
+				return 15;
+		}
+
+		throw new IllegalArgumentException();
 	}
 }
---
+++ src/test/java/org/jlato/unit/tree/LiteralsTest.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright (C) 2015 Didier Villevalois.
+ *
+ * This file is part of JLaTo.
+ *
+ * JLaTo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JLaTo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JLaTo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jlato.unit.tree;
+
+import org.jlato.parser.ParseContext;
+import org.jlato.parser.ParseException;
+import org.jlato.parser.Parser;
+import org.jlato.tree.decl.*;
+import org.jlato.tree.expr.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.jlato.tree.Trees.literalExpr;
+import static org.jlato.tree.Trees.nullLiteralExpr;
+
+/**
+ * @author Didier Villevalois
+ */
+@RunWith(JUnit4.class)
+public class LiteralsTest {
+
+	@Test
+	public void parsedNull() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(null, parseLiteral(parser, "null").value());
+		Assert.assertEquals(nullLiteralExpr(), parseLiteral(parser, "null"));
+	}
+
+	@Test
+	public void parsedBooleans() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(false, parseLiteral(parser, "false").value());
+		Assert.assertEquals(literalExpr(false), parseLiteral(parser, "false"));
+		Assert.assertEquals(true, parseLiteral(parser, "true").value());
+		Assert.assertEquals(literalExpr(true), parseLiteral(parser, "true"));
+	}
+
+	@Test
+	public void parsedIntegers() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42, parseLiteral(parser, "42").value());
+		Assert.assertEquals(literalExpr(42), parseLiteral(parser, "42"));
+	}
+
+	@Test
+	public void parsedHexadecimalIntegers() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(0x42, parseLiteral(parser, "0x42").value());
+		Assert.assertEquals(literalExpr(0x42), parseLiteral(parser, "66"));
+	}
+
+	@Test
+	public void parsedOctalIntegers() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(042, parseLiteral(parser, "042").value());
+		Assert.assertEquals(literalExpr(042), parseLiteral(parser, "34"));
+	}
+
+	@Test
+	public void parsedBinaryIntegers() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42, parseLiteral(parser, "0b00101010").value());
+	}
+
+	@Test
+	public void parsedLongs() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42L, parseLiteral(parser, "42l").value());
+		Assert.assertEquals(42L, parseLiteral(parser, "42L").value());
+		Assert.assertEquals("42l", parseLiteral(parser, "42l").toString());
+		Assert.assertEquals("42L", parseLiteral(parser, "42L").toString());
+		Assert.assertEquals(literalExpr(42L), parseLiteral(parser, "42L"));
+	}
+
+	@Test
+	public void parsedHexadecimalLongs() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(0x42L, parseLiteral(parser, "0x42L").value());
+		Assert.assertEquals(literalExpr(0x42L), parseLiteral(parser, "66L"));
+	}
+
+	@Test
+	public void parsedOctalLongs() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(042L, parseLiteral(parser, "042L").value());
+		Assert.assertEquals(literalExpr(042L), parseLiteral(parser, "34L"));
+	}
+
+	@Test
+	public void parsedBinaryLongs() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42L, parseLiteral(parser, "0b00101010L").value());
+	}
+
+	@Test
+	public void parsedFloats() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42f, parseLiteral(parser, "42f").value());
+		Assert.assertEquals(42F, parseLiteral(parser, "42F").value());
+		Assert.assertEquals(42.42f, parseLiteral(parser, "42.42f").value());
+		Assert.assertEquals(42.42F, parseLiteral(parser, "42.42F").value());
+		Assert.assertEquals("42f", parseLiteral(parser, "42f").toString());
+		Assert.assertEquals("42F", parseLiteral(parser, "42F").toString());
+		Assert.assertEquals("42.42f", parseLiteral(parser, "42.42f").toString());
+		Assert.assertEquals("42.42F", parseLiteral(parser, "42.42F").toString());
+		Assert.assertEquals(literalExpr(42F), parseLiteral(parser, "42.0F"));
+		Assert.assertEquals(literalExpr(42.42F), parseLiteral(parser, "42.42F"));
+	}
+
+	@Test
+	public void parsedHexadecimalFloats() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(0x42p0f, parseLiteral(parser, "0x42p0f").value());
+		Assert.assertEquals(0x42p0F, parseLiteral(parser, "0x42p0F").value());
+		Assert.assertEquals(0x42p24f, parseLiteral(parser, "0x42p24f").value());
+		Assert.assertEquals(0x42p24F, parseLiteral(parser, "0x42p24F").value());
+		Assert.assertEquals("0x42p0f", parseLiteral(parser, "0x42p0f").toString());
+		Assert.assertEquals("0x42p0F", parseLiteral(parser, "0x42p0F").toString());
+		Assert.assertEquals("0x42p24f", parseLiteral(parser, "0x42p24f").toString());
+		Assert.assertEquals("0x42p24F", parseLiteral(parser, "0x42p24F").toString());
+
+		Assert.assertEquals(0x42.42p0f, parseLiteral(parser, "0x42.42p0f").value());
+		Assert.assertEquals(0x42.42p24F, parseLiteral(parser, "0x42.42p24F").value());
+		Assert.assertEquals("0x42.42p0f", parseLiteral(parser, "0x42.42p0f").toString());
+		Assert.assertEquals("0x42.42p24F", parseLiteral(parser, "0x42.42p24F").toString());
+	}
+
+	@Test
+	public void parsedDoubles() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(42d, parseLiteral(parser, "42d").value());
+		Assert.assertEquals(42D, parseLiteral(parser, "42D").value());
+		Assert.assertEquals(42.42d, parseLiteral(parser, "42.42d").value());
+		Assert.assertEquals(42.42D, parseLiteral(parser, "42.42D").value());
+		Assert.assertEquals(42.42, parseLiteral(parser, "42.42").value());
+		Assert.assertEquals(42.42, parseLiteral(parser, "42.42").value());
+		Assert.assertEquals("42d", parseLiteral(parser, "42d").toString());
+		Assert.assertEquals("42D", parseLiteral(parser, "42D").toString());
+		Assert.assertEquals("42.42d", parseLiteral(parser, "42.42d").toString());
+		Assert.assertEquals("42.42D", parseLiteral(parser, "42.42D").toString());
+		Assert.assertEquals("42.42", parseLiteral(parser, "42.42").toString());
+		Assert.assertEquals("42.42", parseLiteral(parser, "42.42").toString());
+		Assert.assertEquals(literalExpr(42D), parseLiteral(parser, "42.0"));
+		Assert.assertEquals(literalExpr(42.42D), parseLiteral(parser, "42.42"));
+		Assert.assertEquals(literalExpr(42.42), parseLiteral(parser, "42.42"));
+	}
+
+	@Test
+	public void parsedHexadecimalDoubles() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals(0x42p0d, parseLiteral(parser, "0x42p0d").value());
+		Assert.assertEquals(0x42p0D, parseLiteral(parser, "0x42p0D").value());
+		Assert.assertEquals(0x42p42d, parseLiteral(parser, "0x42p42d").value());
+		Assert.assertEquals(0x42p42D, parseLiteral(parser, "0x42p42D").value());
+		Assert.assertEquals("0x42p0d", parseLiteral(parser, "0x42p0d").toString());
+		Assert.assertEquals("0x42p0D", parseLiteral(parser, "0x42p0D").toString());
+		Assert.assertEquals("0x42p42d", parseLiteral(parser, "0x42p42d").toString());
+		Assert.assertEquals("0x42p42D", parseLiteral(parser, "0x42p42D").toString());
+
+		Assert.assertEquals(0x42.42p0d, parseLiteral(parser, "0x42.42p0d").value());
+		Assert.assertEquals(0x42.42p42D, parseLiteral(parser, "0x42.42p42D").value());
+		Assert.assertEquals("0x42.42p0d", parseLiteral(parser, "0x42.42p0d").toString());
+		Assert.assertEquals("0x42.42p42D", parseLiteral(parser, "0x42.42p42D").toString());
+	}
+
+	@Test
+	public void parsedCharacter() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals('a', parseLiteral(parser, "'a'").value());
+		Assert.assertEquals(literalExpr('a'), parseLiteral(parser, "'a'"));
+		Assert.assertEquals("'a'", parseLiteral(parser, "'a'").toString());
+
+		Assert.assertEquals('\u0042', parseLiteral(parser, "'\\u0042'").value());
+		Assert.assertEquals(literalExpr('\u0042'), parseLiteral(parser, "'B'"));
+		Assert.assertEquals("'\\u0042'", parseLiteral(parser, "'\\u0042'").toString());
+
+		Assert.assertEquals('\042', parseLiteral(parser, "'\\042'").value());
+		Assert.assertEquals(literalExpr('\042'), parseLiteral(parser, "'\\\"'"));
+		Assert.assertEquals("'\\042'", parseLiteral(parser, "'\\042'").toString());
+
+		Assert.assertEquals('\t', parseLiteral(parser, "'\\t'").value());
+		Assert.assertEquals(literalExpr('\t'), parseLiteral(parser, "'\\t'"));
+		Assert.assertEquals("'\\t'", parseLiteral(parser, "'\\t'").toString());
+
+		Assert.assertEquals('\b', parseLiteral(parser, "'\\b'").value());
+		Assert.assertEquals(literalExpr('\b'), parseLiteral(parser, "'\\b'"));
+		Assert.assertEquals("'\\b'", parseLiteral(parser, "'\\b'").toString());
+
+		Assert.assertEquals('\n', parseLiteral(parser, "'\\n'").value());
+		Assert.assertEquals(literalExpr('\n'), parseLiteral(parser, "'\\n'"));
+		Assert.assertEquals("'\\n'", parseLiteral(parser, "'\\n'").toString());
+
+		Assert.assertEquals('\r', parseLiteral(parser, "'\\r'").value());
+		Assert.assertEquals(literalExpr('\r'), parseLiteral(parser, "'\\r'"));
+		Assert.assertEquals("'\\r'", parseLiteral(parser, "'\\r'").toString());
+
+		Assert.assertEquals('\f', parseLiteral(parser, "'\\f'").value());
+		Assert.assertEquals(literalExpr('\f'), parseLiteral(parser, "'\\f'"));
+		Assert.assertEquals("'\\f'", parseLiteral(parser, "'\\f'").toString());
+
+		Assert.assertEquals('\'', parseLiteral(parser, "'\\''").value());
+		Assert.assertEquals(literalExpr('\''), parseLiteral(parser, "'\\''"));
+		Assert.assertEquals("'\\''", parseLiteral(parser, "'\\''").toString());
+
+		Assert.assertEquals('\"', parseLiteral(parser, "'\\\"'").value());
+		Assert.assertEquals(literalExpr('\"'), parseLiteral(parser, "'\\\"'"));
+		Assert.assertEquals("'\\\"'", parseLiteral(parser, "'\\\"'").toString());
+
+		Assert.assertEquals('\\', parseLiteral(parser, "'\\\\'").value());
+		Assert.assertEquals(literalExpr('\\'), parseLiteral(parser, "'\\\\'"));
+		Assert.assertEquals("'\\\\'", parseLiteral(parser, "'\\\\'").toString());
+	}
+
+	@Test
+	public void parsedString() throws ParseException {
+		Parser parser = new Parser();
+
+		Assert.assertEquals("abcdefgh", parseLiteral(parser, "\"abcdefgh\"").value());
+		Assert.assertEquals(literalExpr("abcdefgh"), parseLiteral(parser, "\"abcdefgh\""));
+		Assert.assertEquals("\"abcdefgh\"", parseLiteral(parser, "\"abcdefgh\"").toString());
+
+		Assert.assertEquals("abcdefgh", parseLiteral(parser, "\"a\\u0062c\\u0064e\\u0066g\\u0068\"").value());
+		Assert.assertEquals(literalExpr("a\u0062c\u0064e\u0066g\u0068"), parseLiteral(parser, "\"a\u0062c\u0064e\u0066g\u0068\""));
+		Assert.assertEquals("\"a\\u0062c\\u0064e\\u0066g\\u0068\"", parseLiteral(parser, "\"a\\u0062c\\u0064e\\u0066g\\u0068\"").toString());
+
+		Assert.assertEquals("\t\b\n\r\f\'\"\\", parseLiteral(parser, "\"\\t\\b\\n\\r\\f\\'\\\"\\\\\"").value());
+		Assert.assertEquals(literalExpr("\t\b\n\r\f\'\"\\"), parseLiteral(parser, "\"\\t\\b\\n\\r\\f\\'\\\"\\\\\""));
+		Assert.assertEquals("\"\\t\\b\\n\\r\\f\\'\\\"\\\\\"", parseLiteral(parser, "\"\\t\\b\\n\\r\\f\\'\\\"\\\\\"").toString());
+	}
+
+	private LiteralExpr parseLiteral(Parser parser, String string) throws ParseException {
+		return (LiteralExpr) parser.parse(ParseContext.Expression, string);
+	}
+}
