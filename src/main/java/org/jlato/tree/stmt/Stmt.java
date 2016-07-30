10452d6caf76b27f1ef98780cd661bfc44f16d84
Didier Villevalois <ptitjes@free.fr>
2016-07-30 16:55:56 +0200

    [Copyright] Update copyright notices

--- src/main/java/org/jlato/tree/stmt/Stmt.java
+++ src/main/java/org/jlato/tree/stmt/Stmt.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015-2016 Didier Villevalois.
+ *
+ * This file is part of JLaTo.
+ *
+ * JLaTo is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * JLaTo is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JLaTo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.jlato.tree.stmt;
 
 import org.jlato.tree.Node;
